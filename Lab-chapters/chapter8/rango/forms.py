66bdee4ac4f73d0a99f070841ade80b89c2d22cc
Bo Li <2435638L@student.gla.ac.uk>
2020-02-16 00:13:22 +0000

    chapter8 Lab exercise

---
+++ Lab-chapters/chapter8/rango/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from rango.models import Page, Category
+
+class CategoryForm(forms.ModelForm):
+      name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
+      # Set the field to zero by default
+      # Fields are hidden that users cannot enter a value.
+      views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+      likes = forms.IntegerField(widget=forms.HiddenInput(),initial=0)
+      slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+      # An inline class to provide additional information on the form
+      # Meta class to specify which fields we wish to include in the form through the fields tuple.
+      class Meta:
+           # Provide an association between the ModelForm and a model
+           model = Category
+           fields = ('name',)
+
+class PageForm(forms.ModelForm):
+      title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
+      url = forms.URLField(max_length=Page.URL_MAX_LENGTH, help_text="Please enter the URL of the page.")
+      views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+     
+      class Meta:
+           model = Page
+           exclude = ('category',)
+
+      def clean(self):
+           cleaned_data = self.cleaned_data
+        # get() method to obtain the form's values
+        # here, get() method would return None rather thab raise a KeyError exception   
+           url = cleaned_data.get('url')
+        # If url is not empty but not start with 'http://',
+        # then prepend 'http://'
+           if url and not url.startswith('http://'):
+                url = f'http://{url}'
+                cleaned_data['url'] = url 
+        # teturn the reference to the cleaned_data dictionary 
+           return cleaned_data
---
+++ Lab-chapters/chapter8/rango/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+
+    # CharField: a field for storing character data
+    # URLField for storing resource URLs
+    # IntegerField stores integers
+    # DateField stores a Python datetime.date object
+    # each category name must be unique and be used the field as a primary key
+
+class Category(models.Model):
+    # to avoid repetition
+    NAME_MAX_LENGTH = 128
+
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
